bd32e8c6a23ec253fab0fcde6fed7b42db104766
rvelloso <rvelloso@tjsc.jus.br>
2019-02-04 18:32:09 -0200

    update datasize inside playing loop (datasize may change no the fly if soundfile is truncated)

--- linux/Sound/AlsaHandle.cpp
+++ linux/Sound/AlsaHandle.cpp
@@ -104,6 +104,8 @@ void AlsaHandle::play() {
 			size_t numFrames = decoder->getDataSize() / frameSize;
 
 			while (numFrames > 0 && playing) {
+				size_t numFrames = decoder->getDataSize() / frameSize;
+
 				auto frames = std::min(numFrames, minFrames);
 				auto bytes = decoder->getSamples(reinterpret_cast<char *>(volBuf.get()), position, frames*frameSize);
 
