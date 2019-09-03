e705ba05bf4f640471d29e01e4344361d6a3b387
roberto <rvelloso@gmail.com>
2019-09-03 10:15:32 -0300

    decorators that change sound properties (sample rate, bits per sample, # channels) are not allowed during playback

--- jukebox/Sound/Sound.cpp
+++ jukebox/Sound/Sound.cpp
@@ -118,8 +118,13 @@ Sound& Sound::fade(int fadeInSecs, int fadeOutSecs) {
 	return *this;
 }
 
+/* changing sound properties (resolution, sample
+ * rate, channels) is not allowed while playing
+ */
 Sound& Sound::resolution(int bitsPerSample) {
-	impl->getDecoder().wrap<SampleResolutionImpl>(bitsPerSample);
+	if (!playing()) {
+		impl->getDecoder().wrap<SampleResolutionImpl>(bitsPerSample);
+	} // TODO: throw?
 	return *this;
 }
 
@@ -163,9 +168,9 @@ Sound& Sound::stop() {
 }
 
 Sound& Sound::jointStereo() {
-	if (getNumChannels() == 2) {
+	if (getNumChannels() == 2 && !playing()) {
 		impl->getDecoder().wrap<JointStereoImpl>();
-	}
+	} // TODO: throw?
 	return *this;
 }
 
