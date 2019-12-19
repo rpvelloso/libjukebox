228eda786c4b054ba85ad94715a4d3fe6fd3da92
thinlizzy <jose.diego@gmail.com>
2019-12-18 21:33:58 -0500

    fixed headers

--- jukebox_test/demo/play.cpp
+++ jukebox_test/demo/play.cpp
@@ -13,8 +13,8 @@
     along with libjukebox.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <array>
 #include <iostream>
-#include <algorithm>
 #include <exception>
 #include "libjukebox.h"
 
